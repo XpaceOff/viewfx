b9c0ba3450f2a437bd81b847764403ecc004580f
Ricx8 <marquessricardo@gmail.com>
2022-04-03 23:19:59 -0400

    playing videos with bitmap is working

--- viewvfx_bitmap.js
+++ viewvfx_bitmap.js
@@ -1,6 +1,7 @@
+let frames = [];
+let nFrame = 0;
 
 document.addEventListener("DOMContentLoaded", () => {
-    const frames = [];
     let tmpCanvas, tmpCanvasContext; 
     let mediaSource01 = "http://upload.wikimedia.org/wikipedia/commons/7/79/Big_Buck_Bunny_small.ogv";
     let mediaSource02 = "media/pro01Blur.mp4";
@@ -21,16 +22,17 @@ document.addEventListener("DOMContentLoaded", () => {
     // As some additional info is needed we will place the video in a
     // containing object for convenience
     video.autoPlay = false; // ensure that the video does not auto play
-    video.loop = true; // set the video to loop.
+    video.loop = false; // set the video to not loop.
     video.muted = muted;
 
     video02.autoPlay = false;
-    video02.loop = true;
+    video02.loop = false;
     video02.muted = true;
 
     videoContainer = {  // we will add properties as needed
         video : video,
-        ready : false,   
+        ready : false,
+        isPause : true 
     };
     videoContainer02 = {  // we will add properties as needed
         video : video02,
@@ -38,40 +40,81 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     //video02.oncanplay = readyToPlayVideo02;
-    //video.oncanplay = readyToPlayVideo; // set the event to the play function that can be found below
+    video.oncanplay = readyToPlayVideo; // set the event to the play function that can be found below
     
     function readyToPlayVideo02(event){
         videoContainer02.ready = true;
     }
 
-    function readyToPlayVideo(event){ // this is a referance to the video
-        console.log("READY");
-        // the video may not match the canvas size so find a scale to fit
-        videoContainer.scale = Math.min(
-            canvas.width / this.videoWidth, 
-            canvas.height / this.videoHeight
-        );
+    async function getVideoTrack() {
+        await videoContainer.video.play();
+        const [track] = video.captureStream().getVideoTracks();
+        video.onended = (evt) => track.stop();
+        return track;
+    }
 
-        videoContainer.ready = true;
-        // the video can be played so hand it off to the display function
-        requestAnimationFrame(updateCanvas);
+    async function readyToPlayVideo(event){ // this is a referance to the video
+        if (window.MediaStreamTrackProcessor) {
+            console.log("MediaStreamTrackProcessor");
+            const track = await getVideoTrack();
+            const processor = new MediaStreamTrackProcessor(track);
+            const reader = processor.readable.getReader();
+            readChunk();
+        
+            function readChunk() {
+                reader.read().then(async({ done, value }) => {
+                    console.log(done);
+                    if (value) {
+                        const bitmap = await createImageBitmap(value);
+                        const index = frames.length;
+                        frames.push(bitmap);
+                        //select.append(new Option("Frame #" + (index + 1), index));
+                        value.close();
+                    }
+                    if (!done) {
+                        readChunk();
+                    } else {
+                        // Once image is loaded into buffer
+                        console.log("Loaded!");
+                        console.log(frames.length, " frames.");
+
+                        // the video may not match the canvas size so find a scale to fit
+                        videoContainer.scale = Math.min(
+                            canvas.width / this.videoWidth, 
+                            canvas.height / this.videoHeight
+                        );
+
+                        videoContainer.ready = true;
+                        // the video can be played so hand it off to the display function
+                        requestAnimationFrame(updateCanvas);
+                    }
+                });
+            }
+            //playPauseClick();
+        } else {
+          console.error("your browser doesn't support this API yet");
+        }
     }
 
     function updateCanvas(){
         canvasContext.clearRect(0,0,canvas.width,canvas.height);
 
         // only draw if loaded and ready
-        if(videoContainer !== undefined && videoContainer.ready && videoContainer02.ready){ 
+        if(videoContainer !== undefined && videoContainer.ready){ 
             // find the top left of the video on the canvas
-            video.muted = muted;
+            //video.muted = muted;
             var scale = videoContainer.scale;
             var vidH = videoContainer.video.videoHeight;
             var vidW = videoContainer.video.videoWidth;
             var top = canvas.height / 2 - (vidH /2 ) * scale;
             var left = canvas.width / 2 - (vidW /2 ) * scale;
-            
+
+            console.log("loading frame ", nFrame);
+            const frame = frames[nFrame];
+
             // now just draw the video the correct size
-            canvasContext.drawImage(videoContainer.video, left, top, vidW * scale, vidH * scale);
+            //canvasContext.drawImage(frame, left, top, vidW * scale, vidH * scale);
+            canvasContext.drawImage(frame, 0, 0, 800, 400)//, vidW*scale, vidH);
 
             // Black and White Filter
             let mixAmount = 1; // filter mix
@@ -82,15 +125,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
             // Diff Code
             //canvasContext.globalAlpha = mixAmount;   // amount of FX
-            canvasContext.globalCompositeOperation = "difference"; 
-            canvasContext.drawImage(videoContainer02.video, left, top, vidW * scale, vidH * scale);
+            //canvasContext.globalCompositeOperation = "difference"; 
+            //canvasContext.drawImage(videoContainer02.video, left, top, vidW * scale, vidH * scale);
             //canvasContext.drawImage(canvasContext.canvas, left, top, vidW * scale, vidH * scale);
 
             // Get Icon back after filter
-            //canvasContext.globalAlpha = 1;  // reset alpha
-            //canvasContext.globalCompositeOperation = "source-over";  // reset comp*/
+            canvasContext.globalAlpha = 1;  // reset alpha
+            canvasContext.globalCompositeOperation = "source-over";  // reset comp*/
 
-            if(videoContainer.video.paused){ // if not playing show the paused screen 
+            if (!videoContainer.isPause){
+                if (nFrame < frames.length-1){
+                    nFrame = nFrame + 1;
+                }
+                else{
+                    console.log("PAUSED");
+                    nFrame = 1;
+                    videoContainer.isPause = true;
+                }
+            }
+
+            if(videoContainer.isPause){ // if not playing show the paused screen 
                 drawPlayIcon();
             }
         }
@@ -116,53 +170,15 @@ document.addEventListener("DOMContentLoaded", () => {
     }    
 
     function playPauseClick(){
-        if(videoContainer !== undefined && videoContainer.ready && videoContainer02.ready){
-            if(videoContainer.video.paused){                                 
-                videoContainer.video.play();
-                videoContainer02.video.play();
+        if(videoContainer !== undefined && videoContainer.ready){
+            if(videoContainer.isPause){                                 
+                videoContainer.isPause = false;
             }else{
-                videoContainer.video.pause();
-                videoContainer02.video.pause();
+                videoContainer.isPause = true;
             }
         }
-    }
-
-    async function getVideoTrack() {
-        await videoContainer.video.play();
-        const [track] = video.captureStream().getVideoTracks();
-        video.onended = (evt) => track.stop();
-        return track;
     }
 
     // register the event
-    canvas.onclick = async(evt) => {
-        if (window.MediaStreamTrackProcessor) {
-            let stopped = false;
-            const track = await getVideoTrack();
-            const processor = new MediaStreamTrackProcessor(track);
-            const reader = processor.readable.getReader();
-            readChunk();
-        
-            function readChunk() {
-                reader.read().then(async({ done, value }) => {
-                    if (value) {
-                        const bitmap = await createImageBitmap(value);
-                        const index = frames.length;
-                        frames.push(bitmap);
-                        //select.append(new Option("Frame #" + (index + 1), index));
-                        value.close();
-                    }
-                    if (!done) {
-                        console.log("Loaded!");
-                        readChunk();
-                    } else {
-                        select.disabled = false;
-                    }
-                });
-            }
-            //playPauseClick();
-        } else {
-          console.error("your browser doesn't support this API yet");
-        }
-    };
+    canvas.addEventListener("click",playPauseClick);
 });
