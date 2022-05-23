886af730fbfa324bbe7d005f34b569932f28e450
Ricx8 <marquessricardo@gmail.com>
2022-05-23 18:35:21 -0400

    Video bar. Initial version added

---
+++ Tauri/src/bar.svelte
@@ -0,0 +1,28 @@
+<script>
+    let videoCacheProgress = [-1, -1 , 1, 0, 1, 1, -1, 1, -1];
+</script>
+
+<div class="flex flex-col w-full h-full">
+    <div class="flex w-full h-1/3 py-0.5">
+        <div class="flex w-full h-full bg-zinc-900 opacity-50 rounded-sm border border-black drop-shadow-lg">
+            <div class="flex flex-col w-full h-full">
+                <div class="flex w-full h-4/5"></div>
+
+                <!-- Cache progress bar -->
+                <div class="flex flex-row w-full h-1/5">
+                    {#each videoCacheProgress as frameNumber, i}
+                        {#if frameNumber>-1}
+                            <div class="flex w-full bg-green-700">{frameNumber}</div>
+                        {:else}
+                            <div class="flex w-full bg-zinc-700"></div>
+                        {/if}
+                    {/each}
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div class="flex w-full h-2/3 bg-zinc-900 rounded-b-lg text-sm">
+        Buttons Area
+    </div>
+</div>
