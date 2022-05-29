31acb0432f844c254d913a3ee38b737d9775b088
Ricx8 <marquessricardo@gmail.com>
2022-05-29 13:11:28 -0400

    Video Bar fixed to match n frames

---
+++ Tauri/src/Playback/Bar/CacheBarUnit.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let cacheStatus=0;
+    export let corner=false;
+</script>
+
+{#if cacheStatus==2}
+    <div class="flex {corner ? 'w-1/2' : 'w-full'} bg-green-400"></div>
+{:else if cacheStatus==1}
+    <div class="flex {corner ? 'w-1/2' : 'w-full'} bg-yellow-400"></div>
+{:else}
+    <div class="flex {corner ? 'w-1/2' : 'w-full'} bg-zinc-700"></div>
+{/if}
--- Tauri/src/bar.svelte
+++ Tauri/src/bar.svelte
@@ -1,49 +1,46 @@
 <script>
     import VideoControlArea from './VideoControlArea.svelte';
+    import CacheBarUnit from './Playback/Bar/CacheBarUnit.svelte';
     import { barFrameCacheStatus, videoTotalFrameLength, videoCurrentFrame } from './stores'
+    
 </script>
 
 <div class="flex flex-col w-full h-full">
     <div class="flex w-full h-2/6 py-0.5">
+        <div class="flex w-12 h-full">.</div>
+
         <div class="flex w-full h-full bg-zinc-900  rounded-sm border border-black drop-shadow-lg">
             <div class="flex-none flex-col w-full h-full overflow-hidden">
+
+                <!-- Frame Numbers -->
                 <div class="flex w-full h-4/6 px-2">
                     {#each $barFrameCacheStatus as frameNumber, i}
-                        {#if i==$videoTotalFrameLength-1}
-                            <div class="flex w-full items-center justify-between text-sm text-zinc-400">
-                                <div class=" -ml-1">{i}</div>
-                                <div class=" -mr-1">{i+1}</div>
-                            </div>
-                        {:else}
-                            <div class="flex w-full items-center text-sm {i == $videoCurrentFrame ? 'text-purple-400' : 'text-zinc-400'}">
-                                <div class=" -ml-1">{i}</div>
-                            </div>
-                        {/if}
+                        <div class="flex w-full items-center justify-center text-sm {i == $videoCurrentFrame ? 'text-purple-400' : 'text-zinc-400'}">
+                            <div class=" -ml-1">{i}</div>
+                        </div>
                     {/each}
                 </div>
 
                 <!-- Frame lines and color bar cache Status-->
                 <div class="flex flex-col w-full h-2/6">
+
                     <!-- Frame lines -->
                     <div class="flex w-full h-5/6 px-2">
                         {#each $barFrameCacheStatus as frameNumber, i}
-                            {#if i==$videoTotalFrameLength-1}
-                                <div class="flex w-full border-l border-x"></div>
-                            {:else}
-                                <div class="flex w-full border-l"></div>
-                            {/if}
+                            <div class="flex w-full"></div>
+                            <div class="flex w-full border-l"></div>
                         {/each}
                     </div>
 
                     <!-- Cache progress bar -->
                     <div class="flex flex-row w-full h-1/6 px-2 opacity-90">
-                        {#each $barFrameCacheStatus as frameNumber, i}
-                            {#if frameNumber==2}
-                                <div class="flex w-full bg-green-400"></div>
-                            {:else if frameNumber==1}
-                                <div class="flex w-full bg-yellow-400"></div>
+                        {#each $barFrameCacheStatus as cFrameCacheStatus, i}
+                            {#if i==0}
+                                <CacheBarUnit cacheStatus={cFrameCacheStatus}></CacheBarUnit>
+                            {:else if i==$videoTotalFrameLength-1}
+                                <CacheBarUnit cacheStatus={cFrameCacheStatus}></CacheBarUnit>
                             {:else}
-                                <div class="flex w-full bg-zinc-700"></div>
+                                <CacheBarUnit cacheStatus={cFrameCacheStatus}></CacheBarUnit>
                             {/if}
                         {/each}
                     </div>
@@ -52,6 +49,8 @@
 
             </div>
         </div>
+
+        <div class="flex w-12 h-full">.</div>
 
     </div>
     <div class="flex w-full h-4/6 bg-zinc-800 p-1 rounded-b-lg">
