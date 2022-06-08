7030d76e93baefe5707688dc62c60653bd68556c
Ricx8 <marquessricardo@gmail.com>
2022-06-07 21:15:46 -0400

    Buttons updated to be disabled

--- Tauri/src/UpperSide/Buttons/UpButtonText.svelte
+++ Tauri/src/UpperSide/Buttons/UpButtonText.svelte
@@ -1,11 +1,22 @@
 <script>
     export let text;
     export let isPress = false;
+    export let isDisabled = false;
 </script>
 
-<div
-    on:click
-    class="{isPress ? 'bg-zinc-900 scale-95' : 'bg-zinc-800 drop-shadow-xl'} flex w-7 h-6 p-1 mx-1 shadow-md rounded-md items-center text-zinc-600 hover:text-zinc-400 hover:scale-95 active:bg-zinc-900 active:text-zinc-300 active:scale-75"
->
-    <p class=" select-none text-sm">{text}</p>
-</div>
+{#if isDisabled}
+    <div
+        disabled={isDisabled}
+        class="flex w-7 h-6 p-1 mx-1 shadow-md rounded-md items-center text-zinc-600 opacity-30"
+    >
+        <p class=" select-none text-sm">{text}</p>
+    </div>
+{:else}
+    <div
+        on:click
+        disabled={isDisabled}
+        class="{isPress ? 'bg-zinc-900 scale-95' : 'bg-zinc-800 drop-shadow-xl'} flex w-7 h-6 p-1 mx-1 shadow-md rounded-md items-center text-zinc-600 hover:text-zinc-400 hover:scale-95 active:bg-zinc-900 active:text-zinc-300 active:scale-75"
+    >
+        <p class=" select-none text-sm">{text}</p>
+    </div>
+{/if}
--- Tauri/src/UpperSide/Buttons/UpStdButton.svelte
+++ Tauri/src/UpperSide/Buttons/UpStdButton.svelte
@@ -1,11 +1,21 @@
 <script>
     export let cssIcon;
     export let isPress = false;
+    export let isDisabled = false;
 </script>
 
-<div
-    on:click
-    class="{isPress ? 'bg-zinc-900 scale-95' : 'bg-zinc-800 drop-shadow-xl'} flex w-7 h-6 p-1 mx-1 shadow-md rounded-md items-center text-zinc-600 hover:text-zinc-400 hover:scale-95 active:bg-zinc-900 active:text-zinc-300 active:scale-75"
->
-    <i class="fa-solid {cssIcon}  w-full h-full "></i>
-</div>
+{#if isDisabled}
+    <div
+        class="flex w-7 h-6 p-1 mx-1 shadow-md rounded-md items-center text-zinc-600 bg-zinc-800 drop-shadow-xl opacity-30"
+    >
+        <i class="fa-solid {cssIcon}  w-full h-full "></i>
+    </div>
+{:else}
+    <div
+        on:click
+        disabled={isDisabled}
+        class="{isPress ? 'bg-zinc-900 scale-95' : 'bg-zinc-800 drop-shadow-xl'} flex w-7 h-6 p-1 mx-1 shadow-md rounded-md items-center text-zinc-600 hover:text-zinc-400 hover:scale-95 active:bg-zinc-900 active:text-zinc-300 active:scale-75"
+    >
+        <i class="fa-solid {cssIcon}  w-full h-full "></i>
+    </div>
+{/if}
