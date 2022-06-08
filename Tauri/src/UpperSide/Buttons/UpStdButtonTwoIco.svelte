f7448f3e19eb1e69d134eb908df6b12aa8de598e
Ricx8 <marquessricardo@gmail.com>
2022-06-07 21:00:37 -0400

    Button updated to be disabled

--- Tauri/src/UpperSide/Buttons/UpStdButtonTwoIco.svelte
+++ Tauri/src/UpperSide/Buttons/UpStdButtonTwoIco.svelte
@@ -1,12 +1,23 @@
 <script>
     export let cssIcon01;
     export let cssIcon02;
+    export let isDisabled = false;
 </script>
 
-<div
-    on:click
-    class="flex w-9 h-6 p-1 mx-1 shadow-md drop-shadow-xl bg-zinc-800 rounded-md items-center text-zinc-600 hover:text-zinc-400 hover:scale-95 active:bg-zinc-900 active:text-zinc-300 active:scale-75"
->
-    <i class="fa-solid {cssIcon01} text-zinc-500 w-2/3"></i>
-    <i class="fa-solid {cssIcon02} text-zinc-600 w-1/3 ml-0.5"></i>
-</div>
+
+{#if isDisabled}
+    <div
+        class="flex w-9 h-6 p-1 mx-1 shadow-md drop-shadow-xl bg-zinc-800 rounded-md items-center text-zinc-600 opacity-30"
+    >
+        <i class="fa-solid {cssIcon01} text-zinc-500 w-2/3"></i>
+        <i class="fa-solid {cssIcon02} text-zinc-600 w-1/3 ml-0.5"></i>
+    </div>
+{:else}
+    <div
+        on:click
+        class="flex w-9 h-6 p-1 mx-1 shadow-md drop-shadow-xl bg-zinc-800 rounded-md items-center text-zinc-600 hover:text-zinc-400 hover:scale-95 active:bg-zinc-900 active:text-zinc-300 active:scale-75"
+    >
+        <i class="fa-solid {cssIcon01} text-zinc-500 w-2/3"></i>
+        <i class="fa-solid {cssIcon02} text-zinc-600 w-1/3 ml-0.5"></i>
+    </div>
+{/if}
