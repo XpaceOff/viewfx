804ed1e1bc233a95d0ffb90075d5d8f648ec18bc
Ricx8 <marquessricardo@gmail.com>
2022-06-16 23:56:54 -0400

    regex test

--- Tests/ffmpeg-pipe-test/src/main.rs
+++ Tests/ffmpeg-pipe-test/src/main.rs
@@ -1,6 +1,14 @@
+use core::panic;
 use std::process::{Command, Stdio};
 use std::str;
-use std::io::BufReader;
+use regex::Regex;
+//use std::io::BufReader;
+
+struct ImgMetadata {
+    width: u32,
+    height: u32,
+    fps: u16,
+}
 
 fn main() {
 
@@ -24,7 +32,7 @@ fn main() {
         .expect("Failed");*/
 
     println!("# Calling CMD");
-    let mut cmd = Command::new("tmp/ffmpeg.exe")
+    let cmd = Command::new("tmp/ffmpeg.exe")
         .arg("-i")
         .arg("tmp/SC_07.mov")
         .arg("-vf")
@@ -42,10 +50,37 @@ fn main() {
     println!("# CMD called. Waiting...");
 
     let tmp_pipe = cmd.wait_with_output().expect("Error wjile waiting");
+
+    let err_out = match str::from_utf8(&tmp_pipe.stderr) {
+        Ok(out) => {out},
+        Err(err) => panic!("Error: {:?}", err),
+    };
+
+    let err_out = err_out.lines();
+
     println!("# Result --> ");
     println!("Status: {:?}", tmp_pipe.status);
     //println!("Stdout: {:?}", tmp_pipe.stdout);
-    println!("Stderr: {:?}", str::from_utf8(&tmp_pipe.stderr) );
+    //println!("Stderr: {:?}", err_out);
+
+    let rx_filter = Regex::new(r" (\d+)x(\d+),([[:ascii:]]+) (\d+) fps").unwrap();
+    for n_line in err_out {
+        if n_line.contains("Stream "){
+
+            println!("{:?}", n_line);
+            let tmp_meta = match rx_filter.captures(n_line) {
+                Some(r) => match r.get(1) {
+                    Some(r1) => r1.as_str(),
+                    _ => "",
+                },
+                _ => "",
+            };
+
+            println!("{:?}", tmp_meta);
+
+        }
+    }
+
     //println!("All: {:?}", tmp_pipe);
 
     //let cmd_stdout = BufReader::new(cmd.stdout.unwrap());
