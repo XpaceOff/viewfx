38eda607302bcaa1c6a08960b24091ddf2b2fde3
SpaceComet <marquessricardo@gmail.com>
2022-07-04 01:41:42 -0400

    Win size changed. Important notes about cross-origin builts

--- Tauri/src-tauri/src/main.rs
+++ Tauri/src-tauri/src/main.rs
@@ -54,8 +54,9 @@ async fn main() {
       // TODO: You might need a way to get the port that Tauri is running on.
       // If not then the app won't work because of the CORS problem
       CorsLayer::new()
-      .allow_origin("http://localhost:8080".parse::<HeaderValue>().unwrap())
-      //.allow_origin("https://tauri.localhost".parse::<HeaderValue>().unwrap())
+      .allow_origin("http://localhost:8080".parse::<HeaderValue>().unwrap()) // Dev Mode
+      //.allow_origin("https://tauri.localhost".parse::<HeaderValue>().unwrap()) // Win build mode
+      //.allow_origin("tauri://localhost".parse::<HeaderValue>().unwrap()) // Macos built mode
       .allow_methods([Method::GET]),
     );
   
