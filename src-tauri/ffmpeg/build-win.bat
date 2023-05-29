609ff30aa92f5b6446dc9eaee5807d42c9b25c79
Xoffio <38369407+Xoffio@users.noreply.github.com>
2023-05-29 19:06:51 -0400

    updated ffmpeg link for win

--- src-tauri/ffmpeg/build-win.bat
+++ src-tauri/ffmpeg/build-win.bat
@@ -2,11 +2,11 @@
 
 cls
 
-set ffmpeg_name="ffmpeg-n5.0.1-8-g54e0971edb-win64-lgpl-5.0"
+set ffmpeg_name="ffmpeg-n5.1.3-10-g33ed503e59-win64-lgpl-5.1"
 set ffmpeg_sidecar_name="ffmpeg-x86_64-pc-windows-msvc"
 
 echo "# Downloading FFmpeg..."
-powershell.exe -command "& wget https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2022-07-20-12-42/%ffmpeg_name%.zip -O %ffmpeg_name%.zip"
+powershell.exe -command "& wget https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2023-05-29-13-41/%ffmpeg_name%.zip -O %ffmpeg_name%.zip"
 echo "# FFmpeg downloaded successfully!"
 
 echo "# Decompressing FFmpeg..."
